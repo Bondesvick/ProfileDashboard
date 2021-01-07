dba2964839d1442dd1e822024e9e2be20c3deee4
bondesvick <bondesvick@gmail.com>
2021-01-07 22:35:05 +0100

    set up main area

---
+++ src/App.style.js
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+export const MainBody = styled.div`
+  background-color: #262A41;
+  color: #262A41;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 96vh;
+  padding: 1rem
+`
+
+export const Left = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+`
+
+export const Right = styled.div`
+    width: 50%;
+    height: 100%;
+    background-color: #F7F7FF;
+
+    border: 1px solid #707070;
+    border-radius: 28px;
+    opacity: 1; 
+
+`
+
